ce372cb364f59fde691696f5bd9cdf2ea7394aca
chaitanya4559 <chaitanya.k@katomaran.tech>
2025-11-27 22:57:28 +0530

    hghg

--- api/stream.js
+++ api/stream.js
@@ -1,39 +1,6 @@
-import ytdl from "ytdl-core";
-
-export const config = { api: { responseLimit: false } };
-
-export default async function handler(req, res) {
-  try {
-    let { url } = req.query;
-
-    if (!url) return res.status(400).send("Missing URL");
-
-    url = decodeURIComponent(url);
-
-    // Always remove ?si= etc
-    url = url.split("?")[0];
-
-    if (!ytdl.validateURL(url)) {
-      return res.status(400).send("Invalid YouTube URL");
-    }
-
-    const info = await ytdl.getInfo(url);
-    const format = ytdl.chooseFormat(info.formats, {
-      quality: "highestaudio",
-      filter: "audioonly",
-    });
-
-    res.setHeader("Content-Type", "audio/mpeg");
-    res.setHeader("Accept-Ranges", "bytes");
-
-    const stream = ytdl.downloadFromInfo(info, {
-      format,
-      highWaterMark: 1024 * 512, // lower size for vercel streams
-    });
-
-    stream.pipe(res);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Error streaming audio");
-  }
+export default function handler(req, res) {
+  return res.json({
+    received_url: req.query.url,
+    full_query: req.query
+  });
 }
