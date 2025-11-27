57b8d1ca7e84f0bb6431194f6452fb613becf6d6
chaitanya4559 <chaitanya.k@katomaran.tech>
2025-11-27 13:18:35 +0530

    swifyyy

--- api/search.js
+++ api/search.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
   if (!q) return res.status(400).json({ error: "Missing query parameter 'q'" });
 
   try {
-    const results = await YouTube.search(q, { limit: 15 });
+    const results = await YouTube.search(q, { limit: 50 });
 
     // minimal data to send to client
     const mapped = results.map((r) => ({
